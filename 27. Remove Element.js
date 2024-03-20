3e637eeb61da3f882169009a2c9e5397c02e8ed6
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-03-20 15:04:45 +0530

    27-Modified code

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -4,16 +4,25 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    let count = 0
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i,1,2000)
-            continue
-        }else{
-            count++
-        }
+    // let count = 0
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] == val) {
+    //         nums.splice(i,1,2000)
+    //         continue
+    //     }else{
+    //         count++
+    //     }
        
+    // }
+    // nums.sort((a,b)=>a-b)
+    // return count
+
+    //? above code runs fine but lets modify the code bit more
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count++] = nums[i];
+        }
     }
-    nums.sort((a,b)=>a-b)
-    return count
+    return count;
 };
