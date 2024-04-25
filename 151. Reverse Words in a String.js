61079b503961058acd40edb57588494ae427ba93
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-04-25 20:27:37 +0530

    151. Reverse Words in a String

--- 151. Reverse Words in a String.js
+++ 151. Reverse Words in a String.js
@@ -1,17 +1,28 @@
 /**
- * @param {string} s
- * @return {string}
+ * Function to reverse the words in a string.
+ * @param {string} s - Input string.
+ * @return {string} - String with words reversed.
  */
 var reverseWords = function(s) {
-    s = s.trim()
-    s = s.split(" ")
-    let res = ""
-    for(let i=s.length-1;i>=0;i--){
-        if(s[i]==""){
-            
-            continue
+    // Trim any leading or trailing white spaces from the input string
+    s = s.trim();
+
+    // Split the string into an array of words using space as the delimiter
+    s = s.split(" ");
+
+    // Initialize an empty string to store the result
+    let res = "";
+
+    // Iterate over the array of words in reverse order
+    for (let i = s.length - 1; i >= 0; i--) {
+        // If the current word is an empty string, skip it
+        if (s[i] == "") {
+            continue;
         }
-        res+=s[i] + " "
+        // Concatenate the current word and a space to the result string
+        res += s[i] + " ";
     }
-    return res.trim()
+
+    // Trim any trailing white spaces from the result string and return
+    return res.trim();
 };
