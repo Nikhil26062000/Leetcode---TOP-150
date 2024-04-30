5c4a381e5ed0f22b688bc1f53e9777772b1313ec
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-04-30 21:00:11 +0530

    2nd Attempt -------------> 27. Remove Element

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -1,3 +1,24 @@
+
+// --------------------- 2nd Attempt ----------------------------
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let c=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=val){
+          nums[c++]=nums[i]
+        }
+    }
+    return c
+};
+
+
+// ---------------- 1st Attempt -------------------------------
+
 /**
  * @param {number[]} nums
  * @param {number} val
