aa9c947b4376d007b75afe42fcd84294f8effa37
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-05-06 18:45:34 +0530

    2nd Attempt-> Remove Duplicates from Sorted Array.js

--- 26. Remove Duplicates from Sorted Array.js
+++ 26. Remove Duplicates from Sorted Array.js
@@ -7,16 +7,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let count = 1; // Initialize count to 1 since the first element is always unique
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] == nums[count - 1]) {
-            continue; // Skip to the next iteration if the current element is a duplicate
-        } else {
-            nums[count++] = nums[i]; // Store the unique element at the next available index
-        }
+    let c = 1;
+    for(let i=1;i<nums.length;i++){
+          if(nums[i]==nums[c-1]){
+              continue
+          }else{
+              nums[c++] = nums[i]
+          }
     }
-    return count; // Return the count of unique elements
-};
+    return c
+  };
 
 
 
