223c875f781e0d6a6aa6fd7f49697724d22bb813
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-05-09 22:28:30 +0530

    2nd Attempt ------> 189. Rotate Array

--- 189. Rotate Array.js
+++ 189. Rotate Array.js
@@ -1,3 +1,34 @@
+
+
+// -------------------- 2nd Attempt -----------------------
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k %= nums.length;
+    reverseArray(nums,0,nums.length-1)
+    reverseArray(nums,0,k-1)
+    reverseArray(nums,k,nums.length-1)
+};
+
+function reverseArray(nums,start,end){
+    while(start<end){
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+        start++
+        end--
+    }
+
+}
+
+
+
+// ------------------------ 1st Attempt -------------------
+
 /**
  * @param {number[]} nums
  * @param {number} k
