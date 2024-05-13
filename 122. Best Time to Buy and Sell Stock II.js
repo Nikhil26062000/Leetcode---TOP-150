6eca1f792bfb2d7501214ae21eaddf2d044ee02b
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-05-13 17:56:13 +0530

    2nd Atempt --------> 122. Best Time to Buy and Sell Stock II

--- 122. Best Time to Buy and Sell Stock II.js
+++ 122. Best Time to Buy and Sell Stock II.js
@@ -1,3 +1,23 @@
+
+// -------------------- 2nd attempt ----------------
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0
+    for(let i=0;i<prices.length-1;i++){
+        if(prices[i]<prices[i+1]){
+            profit+=(prices[i+1]-prices[i])
+        }
+    }
+    return profit
+};
+
+
+// ------------------ 1st attempt ------------------
+
 /**
  * @param {number[]} prices
  * @return {number}
