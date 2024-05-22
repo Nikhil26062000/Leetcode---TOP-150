71fdd752532633aa6c58e20c82a24f21683bd857
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-05-22 21:39:36 +0530

    2nd Attempt --------> 274. H-Index

--- 274. H-Index.js
+++ 274. H-Index.js
@@ -1,14 +1,37 @@
+
+// ----------2nd Attempt ---------
+
 /**
  * @param {number[]} citations
  * @return {number}
  */
-var hIndex = function(citations) {
+var hIndex = function (citations) {
+
+    citations.sort((a, b) => a - b); //0,1,3,5,6,7
+    let hIndex = 0;
+    for (let i = 0; i < citations.length; i++) {
+        const h = Math.min(citations[i], citations.length - i);
+        hIndex = Math.max(hIndex, h);
+    }
+    return hIndex;
+
+
+};
+
+
+//---------1st Attempt ------------
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
     citations.sort((a, b) => a - b);// 0,1,3,5,6
-      let hIndex = 0;
-      for (let i = 0; i < citations.length; i++) {
-          const h = Math.min(citations[i], citations.length - i);
-          hIndex = Math.max(hIndex, h);
-      }
-      return hIndex;
-  
-  };
+    let hIndex = 0;
+    for (let i = 0; i < citations.length; i++) {
+        const h = Math.min(citations[i], citations.length - i);
+        hIndex = Math.max(hIndex, h);
+    }
+    return hIndex;
+
+};
