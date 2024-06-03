b0e7ad720da21fb07285027889b70e964364effc
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-06-03 20:14:22 +0530

    2nd Attempt -------------->12. Integer to Roman.js

--- 12. Integer to Roman.js
+++ 12. Integer to Roman.js
@@ -1,3 +1,44 @@
+
+
+//? -------------------------------------------------->
+//?     🤞 2nd Attempt
+//? -------------------------------------------------->
+
+/**
+ * Convert an integer to a Roman numeral.
+ * @param {number} num - The input integer to convert.
+ * @return {string} - The Roman numeral representation of the input integer.
+ */
+var intToRoman = function(num) {
+    // Arrays to store the values and corresponding Roman numerals
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    
+    // Result string to store the Roman numeral representation
+    let result = '';
+  
+    // Iterate through the values and numerals arrays
+    for (let i = 0; i < values.length; i++) {
+        // While the input number is greater than or equal to the current value
+        while (num >= values[i]) {
+            // Append the corresponding numeral to the result string
+            result += numerals[i];
+            // Subtract the current value from the input number
+            num -= values[i];
+        }
+    }
+    
+    // Return the final Roman numeral representation
+    return result;
+};
+
+
+
+
+//? ---------------------------------------------------->
+//?    👌 1st Attempt 🔥
+//? ---------------------------------------------------->
+
 /**
  * @param {number} num
  * @return {string}
