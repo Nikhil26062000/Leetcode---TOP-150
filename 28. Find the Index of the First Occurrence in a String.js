60b0efc8753c55dc2ab07526b759c4255ed12b64
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-06-20 21:57:18 +0530

    28. Find the Index of the First Occurrence in a String.js

--- 28. Find the Index of the First Occurrence in a String.js
+++ 28. Find the Index of the First Occurrence in a String.js
@@ -1,14 +1,40 @@
+//? ------------------------------------------------------------->
+//?    ✌️ 2nd Attempt 🔥
+//? ------------------------------------------------------------->
+
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    for(let i=0;i<haystack.length;i++){
-        if(haystack[i]==needle[0]){
-          let temp = haystack.substring(i,needle.length+i)
-          if(temp==needle) return i
-        }
+var strStr = function (haystack, needle) {
+  let flag = false
+  for (let i = 0; i < haystack.length; i++) {
+      if (needle[0] == haystack[i]) {
+          let temp = haystack.substring(i, i + needle.length)
+          if (temp == needle) {
+              return i
+          }
+      }
+  }
+  return -1
+};
+
+//? ------------------------------------------------>
+//?    😎 1st Attempt 😜
+//? ------------------------------------------------>
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] == needle[0]) {
+      let temp = haystack.substring(i, needle.length + i);
+      if (temp == needle) return i;
     }
-    return -1
+  }
+  return -1;
 };
