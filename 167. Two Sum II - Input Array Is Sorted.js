b4cbe76fee505b86554b1883115ba994481a5e7a
Nikhil Raj <76843067+Nikhil26062000@users.noreply.github.com>
2024-06-27 21:47:11 +0530

    167. Two Sum II - Input Array Is Sorted.js

--- 167. Two Sum II - Input Array Is Sorted.js
+++ 167. Two Sum II - Input Array Is Sorted.js
@@ -1,5 +1,27 @@
 
 // ------------------------------------------------------------------->
+// 😎 My own logic 🤞
+// ------------------------------------------------------------------->
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        let complement = target - numbers[i];
+        let ind = numbers.indexOf(complement, i + 1); // start search from the next index
+        if (ind !== -1) {
+            //The condition if (ind !== -1) is used to check if the complement exists in the array. The indexOf method returns -1 if the element is not found. By ensuring that ind is not -1, we confirm that the complement exists and its index is valid.
+            return [i + 1, ind + 1];
+        }
+    }
+};
+
+
+
+// ------------------------------------------------------------------->
 //              😎 1st Attempt 🥰
 // ------------------------------------------------------------------->
 
